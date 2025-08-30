ce5a7c1f7424d8a06618822f641b1e37de198e3f
eastWillow <bobgash2@gmail.com>
2025-08-30 22:54:50 +0800

    feat(testbench): add testbench from spi-master

--- PYNQ-FPGA-ILI9486-Display.srcs/test_button_led/new/test_button_led.v
+++ PYNQ-FPGA-ILI9486-Display.srcs/test_button_led/new/test_button_led.v
@@ -5,7 +5,7 @@
 //
 // Create Date: 08/30/2025 10:08:56 PM
 // Design Name:
-// Module Name: test_spi_master
+// Module Name: test_button_led
 // Project Name:
 // Target Devices:
 // Tool Versions:
@@ -20,7 +20,25 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module test_spi_master(
+module test_button_led(
     );
+    reg clk;
+    reg [3:0] btn;
+    wire [3:0] led;
+
+    top_module top_module_inst(clk, btn, led);
+
+    initial begin
+        clk = 0;         // initialize to zero
+        forever #5 clk = ~clk;  // toggle every 5 ps -> 10 ps period
+    end
+
+    initial begin
+        btn = 0;
+        #10 btn[1] = 1;
+        #10 btn[1] = 0;
+        #10 btn[1] = 1;
+        #10 btn[1] = 0;
+    end
 
 endmodule
